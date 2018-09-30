276756ea026094daec4480007338a9c4aa8baf5c
mingky <thsalsrl7531@naver.com>
2018-09-30 14:12:31 +0000

    1. cloud9 start

---
+++ test3/ccc.java
@@ -0,0 +1,10 @@
+public class ccc {
+    
+    
+     public static void main(String[] args) throws Exception {
+         System.out.println("씨바 fuck you");
+         System.out.println("씨바 fuck you");
+         System.out.println("씨바 fuck you");
+         System.out.println("씨바 fuck you");
+         System.out.println("씨바 fuck you");
+     }}
