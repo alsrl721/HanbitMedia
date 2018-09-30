8c9ce70d54e5218aa8c4f27a3962d336d18a2ab3
mingky <thsalsrl7531@naver.com>
2018-09-30 14:19:06 +0000

    2. add test4

---
+++ test4/ddd.java
@@ -0,0 +1,9 @@
+public class ddd {
+    
+    
+     public static void main(String[] args) throws Exception {
+         System.out.println("son min gi");
+         System.out.println("son min gi");
+         System.out.println("son min gi");
+         System.out.println("son min gi");
+     }}
